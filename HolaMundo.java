759ce3dd04f4da4822a7601a8f85226342daef3f
Jesus Martin Castro <jesmartin@gmail.com>
2016-10-06 11:21:18 +0200

    Comentarios añadidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,4 +1,9 @@
-public class HolaMundo {
+/*
+ * Muestra por pantalla  la frase "¡Hola Mundo!" 
+ * 
+ * @author Jesus Martin Castro
+ */
+  public class HolaMundo {
   public static void main (String [] args){
     System.out.println("¡Hola mundo!");
   }
